eca5ac1bc18a19a0219dc1512d566935f7e42f70
RamandeepSidhu <117151940+RamandeepSidhu@users.noreply.github.com>
2023-12-05 17:27:12 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,56 +1,102 @@
 import React, { useState } from "react";
 import "./App.css";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import Alert from "./components/Alert";
-import { Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
-
-  const showAlert = (message, type) => {
+  const showAlert = (message , type) => {
     setAlert({
-      msg: message,
-      type: type,
-    });
-  };
+      msg:message,
+      type:type
+    })
 
+  }
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
-      document.body.style.backgroundColor = "gray";
-      document.body.style.color = "white";
-      showAlert("Dark mode has been enabled", "Success");
+      document.body.style.backgroundColor='gray';
+      document.body.style.color='white';
+      showAlert("Dark mode has been enabled" ,"Success")
     } else {
       setMode("light");
-      document.body.style.backgroundColor = "white";
-      document.body.style.color = "black";
-      showAlert("Light mode has been enabled", "Success");
+      document.body.style.backgroundColor='white';
+      document.body.style.color='black';
+      showAlert("Light mode has been enabled","Success")
     }
+ 
   };
-
   return (
-    <div >
-    <Navbar title="PixlerLab" mode={mode} toggleMode={toggleMode} />
-              <Alert alert={alert} />
-      <Routes>
-                <Route path="about" element={<About />} />
-                <Route
-                  path="*"
-                  element={
-                    <Textform
-                      heading="Enter Your Daily Status Report "
-                      mode={mode}
-                      toggleMode={toggleMode}
-                      showAlert={showAlert}
-                    />
-                  }
-                />
-      </Routes>
-      </div>
+<>
+      <Navbar title="PixlerLab" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
+      <div className="container my-3">
+          <Textform heading="Enter Your Daily Status Report " mode={mode} toggleMode={toggleMode} showAlert={showAlert}/>
+        </div>
+</> 
   );
 }
 
 export default App;
+
+
+
+// import React, { useState } from "react";
+// import "./App.css";
+// import About from "./components/About";
+// import Navbar from "./components/Navbar";
+// import Textform from "./components/Textform";
+// import Alert from "./components/Alert";
+// import { Routes, Route } from "react-router-dom";
+
+// function App() {
+//   const [mode, setMode] = useState("light");
+//   const [alert, setAlert] = useState(null);
+
+//   const showAlert = (message, type) => {
+//     setAlert({
+//       msg: message,
+//       type: type,
+//     });
+//   };
+
+//   const toggleMode = () => {
+//     if (mode === "light") {
+//       setMode("dark");
+//       document.body.style.backgroundColor = "gray";
+//       document.body.style.color = "white";
+//       showAlert("Dark mode has been enabled", "Success");
+//     } else {
+//       setMode("light");
+//       document.body.style.backgroundColor = "white";
+//       document.body.style.color = "black";
+//       showAlert("Light mode has been enabled", "Success");
+//     }
+//   };
+
+//   return (
+//     <div >
+//     <Navbar title="PixlerLab" mode={mode} toggleMode={toggleMode} />
+//               <Alert alert={alert} />
+//       <Routes>
+//                 <Route path="about" element={<About />} />
+//                 <Route
+//                   path="*"
+//                   element={
+//                     <Textform
+//                       heading="Enter Your Daily Status Report "
+//                       mode={mode}
+//                       toggleMode={toggleMode}
+//                       showAlert={showAlert}
+//                     />
+//                   }
+//                 />
+//       </Routes>
+//       </div>
+//   );
+// }
+
+// export default App;
